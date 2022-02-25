62121a98cf49eee31c1bee576920f35e3d125d7d
Ren0503 <illuminati0440@gmail.com>
2022-02-25 21:08:07 +0700

    Fix

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
-import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
 
-Vue.config.productionTip = false;
+import { createApp, h } from 'vue'
 
-new Vue({
-  store,
-  render: h => h(App)
-}).$mount('#app');
+const app = createApp({ 
+    render: ()=>h(App)
+});
+
+app.use(store);
+
+app.mount('#app');
